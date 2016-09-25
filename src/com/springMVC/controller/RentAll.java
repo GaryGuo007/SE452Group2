3df242b5e314566b9ee349d3c3cadd97e99e7909
bconrad1 <bconrad18@gmail.com>
2016-09-25 17:27:28 -0500

    Update RentAll.java

--- src/com/springMVC/controller/RentAll.java
+++ src/com/springMVC/controller/RentAll.java
@@ -16,7 +16,7 @@ public class RentAll {
 	public ModelAndView helloWorld() {
  
 		String message = "<br><div style='text-align:center;'>"
-				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+				+ "<h3>********** Hello World**********</div><br><br>";
 		return new ModelAndView("testPage", "message", message);
 	}
 }
